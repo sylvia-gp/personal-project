ab2475c9aa46734e61b7c4d4542fa7183f3eb1b7
Student <student@pop-os.localdomain>
2019-08-22 15:16:13 +1200

    add default layout

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const express = require('express')
 const hbs = require('express-handlebars')
 
 const tankRoutes = require('./routes/tank')
+// const tankInfo = require('./routes/tankinfo')
 
 const server = express()
 
 // Middleware
 
-server.engine('hbs', hbs({extname: 'hbs'}))
+server.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main'}))
 server.set('view engine', 'hbs')
 server.use(express.urlencoded({extended: true}))
 
 // Routes
 
 server.use('/', tankRoutes)
+// server.use('/tank/:id', tankInfo)
 
 module.exports = server
