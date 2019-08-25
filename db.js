4b16e80cbd62c3a5abf62c4140169904b1fe8ea6
Sylvia Gilbert-Potts <sylvia.gilbert.potts@hotmail.com>
2019-08-25 17:34:34 +1200

    add fish info to each tanks fish page

--- db.js
+++ db.js
@@ -3,14 +3,9 @@ const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 module.exports = {
-  // getTankInfo: getTankInfo,
   getTanks: getTanks
 }
 
 function getTanks (db = connection) {
   return db('tanks').select()
 }
-
-// function getTankInfo (id, db = connection) {
-//   return db('tanks').where('id', id)
-// }
--- routes/tank.js
+++ routes/tank.js
@@ -2,6 +2,9 @@ const express = require('express')
 const db = require('../db')
 const router = express.Router()
 
+const development = require('../knexfile').development
+const knex = require('knex')(development)
+
 router.get('/', (req, res) => {
     db.getTanks()
     .then(tanks => {
@@ -20,5 +23,16 @@ router.get('/tank:id', (req, res) => {
     })
 })
 
+router.get('/tank:id/fish', (req, res) => {
+    const id = req.params.id
+    knex('fish')
+    .join('tanks', 'fish.tank_id', 'tanks.id')
+    .where({'fish.tank_id': id})
+    .select('image', 'species', 'quantity')
+    .then(function (fishes) {
+        const fish = fishes
+        res.render('partials/fishinfo', {fish: fish})
+    })
+})
 
 module.exports = router
